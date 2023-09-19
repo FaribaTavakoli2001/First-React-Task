8712ddc27b2ffa3214d38a59888cef70b58a1818
FaribaTavakoli2001 <117892836+FaribaTavakoli2001@users.noreply.github.com>
2023-09-20 02:33:29 +0330

    counter-events

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import styles from './Card.module.css';
+import minuse from '../images/minuse.jpg';
+import pluse from '../images/pluse.jpg'
 
 
 class Card extends Component {
+    constructor(){
+        super();
+        this.state = {
+            counter : 0 ,
+        }
+    }
+    downHandler = () => {
+        if(this.state.counter >= 1){
+
+            this.setState (prevstate => ({
+                counter : prevstate.counter -1 ,
+            }))
+        }
+        }
+    upHandler = () => {
+        this.setState (prevstate => ({
+            counter : prevstate.counter + 1 ,
+        }))
+    }
+
     render() {
         const {image , name , price , children } = this.props;
+        const { counter } = this.state ;
         return (
             <div className={styles.container}>
                 <img src={image} alt='smart phone' />
                 <h3>{name} {children}</h3>
-                <p>{price}</p>                
+                <p>{price}</p> 
+                <div className={styles.counter}>
+
+                    <img className={counter ? '': styles.deactive} src={minuse} alt='down' onClick={this.downHandler}/>
+                    <span>{counter}</span>
+                    <img src={pluse} alt='up' onClick={this.upHandler}/>
+
+
+                </div>               
             </div>
-        );
+    );
+        }
     }
-}
 
 export default Card;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,11 +3,27 @@ import styles from './Search.module.css';
 
 
 class Search extends Component {
+    constructor(){
+        super();
+        this.state ={
+            text : '',
+        }
+    }
+    changeHandler = (event) =>{
+         this.setState(
+            {
+                text : event.target.value ,
+            }
+         )
+    }
     render() {
         return (
             <div className={styles.container}>
                 <p>what you want?</p>
-                <input placeholder='Search...'/>
+                <br />
+                <span>{this.state.text}</span>
+                <br />
+                <input value={this.state.text} onChange={this.changeHandler}   placeholder='Search...'/>
                 
             </div>
         );
